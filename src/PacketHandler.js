06451faed5b680c2339f9d30546eafc44ac57cb2
EsaelYT <43244368+EsaelYT@users.noreply.github.com>
2020-12-23 21:51:21 -0400

    Update PacketHandler.js

--- src/PacketHandler.js
+++ src/PacketHandler.js
@@ -72,9 +72,9 @@ PacketHandler.prototype.handshake_onCompleted = function (protocol, key) {
     this.protocol = protocol;
     // Send handshake response
     this.sendPacket(new Packet.ClearAll());
-    this.sendPacket(new Packet.SetBorder(this.socket.playerTracker, this.gameServer.border, this.gameServer.config.serverGamemode, "MultiOgar-Edited " + this.gameServer.version));
+    this.sendPacket(new Packet.SetBorder(this.socket.playerTracker, this.gameServer.border, this.gameServer.config.serverGamemode, "Welcome to Zagar.ml "));
     // Send welcome message
-    this.gameServer.sendChatMessage(null, this.socket.playerTracker, "MultiOgar-Edited " + this.gameServer.version);
+    this.gameServer.sendChatMessage(null, this.socket.playerTracker, "Welcome to Zagar.ml");
     if (this.gameServer.config.serverWelcome1)
         this.gameServer.sendChatMessage(null, this.socket.playerTracker, this.gameServer.config.serverWelcome1);
     if (this.gameServer.config.serverWelcome2)
